fe5f2db956483c016486ff167478462539263af0
System Administrator <root@iMac.local>
2016-01-27 03:29:39 +0200

    Create task 5

--- main.m
+++ main.m
@@ -14,15 +14,13 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        SVMArtist *artistOne = [SVMArtist new];
         SVMSong *songOne = [[SVMSong alloc] initWithSongName:@"Winter" newArtist:@"Joshua Radin" andText:@"I should know\nWho I am by now\nI walk\nThe record stands somehow\nThinking of winter"];
         SVMSong *songTwo = [[SVMSong alloc] initWithSongName:@"Friends Like you" newArtist:@"Joshua Radin" andText:@"I like the way you're not afraid\nYou got the world planned in your mind\nPeople say you cannot do well\nhey don't know a friend like you"];
-        [artistOne setListOfSong:@[songOne,songTwo]];
-        [artistOne setName:@"Joshua Radin"];
-        NSLog(@"%@",[artistOne name]);
-        SVMArtist *artistTwo = [SVMArtist new];
-        [artistTwo setName:@"Brutto"];
-        NSLog(@"%@",[artistTwo name]);
+        SVMSong *songThre = [[SVMSong alloc] initWithSongName:@"Mr. Jack" newArtist:@"SOAD" andText:@"Hey Mr. Jack\nIs that the mouth wash in your eyes?\nHey Mr. Jack\nIs that the cause of your surprise?"];
+        SVMSong *songFour = [[SVMSong alloc] initWithSongName:@"I-E-A-I-A-I-O" newArtist:@"SOAD" andText: @"Peter's pecker picked another\nPickle bearing pussy pepper,\nPeter's pecker picked another\n      Pickle bearing pussy pepper, why?"];
+        SVMArtist *artistOne = [[SVMArtist alloc] initWithName:@"Joshua Radin" andListOfSong:@[songOne,songTwo]];
+        SVMArtist *artistTwo = [[SVMArtist alloc] initWithName:@"SOAD" andListOfSong:@[songThre, songFour]];
+        
         NSArray *array = @[artistOne, artistTwo];
         NSLog(@"%@", [array printPretty]);
         [songTwo play];
